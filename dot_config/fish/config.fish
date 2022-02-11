d312cfc958aa6959a93aea6b49d1fa8d6ad0de25
Dmitry Samsonov <dmitriy.samsonov28@gmail.com>
2022-02-12 05:04:58 +0700

    Initial commit

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,6 @@
+set -gx EDITOR micro
+set -gx MICRO_TRUECOLOR 1
+set -gx VIRTUAL_ENV_DISABLE_PROMPT 1 #For Agnoster theme
+alias rm="rm -v"
+abbr -ag py python
+abbr -ag r ranger
---
+++ run_once_set_cursors_bibata.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+gsettings set org.gnome.desktop.interface cursor-theme Bibata-Modern-Ice
